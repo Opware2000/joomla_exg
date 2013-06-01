3b653ab71eb36009fa0222ad962a108c17e71fed
Nicolas OGIER <nicolas.ogier@gmail.com>
2013-06-02 00:18:26 +0200

    gère dynamiquement le nombre d'images par ligne

--- plugin_exg/exg.class.php
+++ plugin_exg/exg.class.php
@@ -136,15 +136,15 @@ class exgClass {
 		$css .='#gallery_'.$this->_gallerieNombre.' li.images {float:left; margin-left:5px; margin-right:5px; display:block;}';
 		$css .='#gallery_'.$this->_gallerieNombre.' ul.exg { display:block;}';
 		$css .='#gallery_'.$this->_gallerieNombre.' {overflow: hidden; margin: 10px;}';
-		$css .='#gallery_'.$this->_gallerieNombre.' .box {float: left;position: relative; width: 20%; padding-bottom: 20%;}';
+		$css .='#gallery_'.$this->_gallerieNombre.' .box {float: left;position: relative; width: '.(100/$this->_nombreImage[0]).'%; padding-bottom: 20%;}';
 		$css .='#gallery_'.$this->_gallerieNombre.' .boxInner {	position: absolute;	left: 10px;right: 10px;top: 10px;bottom: 10px;overflow: hidden;}';
 		$css .='#gallery_'.$this->_gallerieNombre.' .boxInner img {width: 100%;}';
 		$css .='#gallery_'.$this->_gallerieNombre.' .boxInner .titleBox {position: absolute;	bottom: 0;left: 0;right: 0;margin-bottom: -50px;background: #000;background: rgba(0, 0, 0, 0.5);color: #FFF;	padding: 10px;text-align: center;	-webkit-transition: all 0.3s ease-out;-moz-transition: all 0.3s ease-out;	-o-transition: all 0.3s ease-out;transition: all 0.3s ease-out;}';
 		$css .='#gallery_'.$this->_gallerieNombre.' .boxInner:hover .titleBox { margin-bottom: 0;}';
-		$css .='@media only screen and (max-width : 480px) {/* Smartphone view: 1 tile */ #gallery_'.$this->_gallerieNombre.' .box {width: 100%;padding-bottom: 100%;}}';
-		$css .='@media only screen and (max-width : 650px) and (min-width : 481px)   {/* Tablet view: 2 tiles */#gallery_'.$this->_gallerieNombre.' .box { width: 50%; padding-bottom: 50%;}}';
-		$css .='@media only screen and (max-width : 1050px) and (min-width : 651px)  {/* Small desktop / ipad view: 3 tiles */ #gallery_'.$this->_gallerieNombre.' .box {  width: 33.3%; padding-bottom: 33.3%; }}';
-		$css .='@media only screen and (max-width : 1290px) and (min-width : 1051px) {/* Medium desktop: 4 tiles */   #gallery_'.$this->_gallerieNombre.' .box { width: 25%;  padding-bottom: 25%;}}';
+		$css .='@media only screen and (max-width : 480px) {/* Smartphone view: 1 tile */ #gallery_'.$this->_gallerieNombre.' .box {width: '.(100/$this->_nombreImage[4]).'%;padding-bottom: 100%;}}';
+		$css .='@media only screen and (max-width : 650px) and (min-width : 481px)   {/* Tablet view: 2 tiles */#gallery_'.$this->_gallerieNombre.' .box { width: '.(100/$this->_nombreImage[3]).'%; padding-bottom: 50%;}}';
+		$css .='@media only screen and (max-width : 1050px) and (min-width : 651px)  {/* Small desktop / ipad view: 3 tiles */ #gallery_'.$this->_gallerieNombre.' .box {  width:'.(100/$this->_nombreImage[2]).'%; padding-bottom: 33.3%; }}';
+		$css .='@media only screen and (max-width : 1290px) and (min-width : 1051px) {/* Medium desktop: 4 tiles */   #gallery_'.$this->_gallerieNombre.' .box { width: '.(100/$this->_nombreImage[1]).'%;  padding-bottom: 25%;}}';
 		return $css ;
 	}
 }
